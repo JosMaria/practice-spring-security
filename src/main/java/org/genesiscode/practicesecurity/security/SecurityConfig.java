be4c0f8fe4756e88bbb616a6ef535885dc5b8dad
Jose Maria <josmariaguilar@gmail.com>
2022-03-10 16:39:50 -0400

    In memory user details manager

--- src/main/java/org/genesiscode/practicesecurity/security/SecurityConfig.java
+++ src/main/java/org/genesiscode/practicesecurity/security/SecurityConfig.java
@@ -1,9 +1,14 @@
 package org.genesiscode.practicesecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -17,5 +22,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails maria = User.builder()
+                .username("maria")
+                .password("password")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(maria);
     }
 }

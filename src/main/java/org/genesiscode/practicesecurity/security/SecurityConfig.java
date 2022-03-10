51283df7987405ef7eecc5cfb546d352ffde08e1
Jose Maria <josmariaguilar@gmail.com>
2022-03-10 16:53:33 -0400

    password encoding with bcrypt

--- src/main/java/org/genesiscode/practicesecurity/security/SecurityConfig.java
+++ src/main/java/org/genesiscode/practicesecurity/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.genesiscode.practicesecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         UserDetails maria = User.builder()
                 .username("maria")
-                .password("password")
+                .password(passwordEncoder.encode("password"))
                 .roles("STUDENT")
                 .build();
 
